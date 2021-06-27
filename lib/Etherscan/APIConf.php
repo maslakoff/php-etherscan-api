ee3705dbb415a7f98b32124a03ad7b4be7ded64c
marciuz <marcelloverona@gmail.com>
2021-06-27 15:33:42 +0200

    Added support for Binance Smart Chain API url

--- lib/Etherscan/APIConf.php
+++ lib/Etherscan/APIConf.php
@@ -11,9 +11,13 @@ class APIConf
 {
     const API_URL = "https://api.etherscan.io/api";
 
+    const API_URL_BSCSCAN = 'https://api.bscscan.com/api';
+
     const TESTNET_ROPSTEN = "api-ropsten";
     const TESTNET_KOVAN = "api-kovan";
     const TESTNET_RINKEBY = "api-rinkeby";
+
+    const NET_BSCSCAN = 'api-bscscan'; 
 
     const TAG_LATEST = "latest";
 
@@ -29,6 +33,9 @@ class APIConf
     public static function getAPIUrl($net = null) {
         if (is_null($net)) {
             return self::API_URL;
+        }
+        else if($net === self::NET_BSCSCAN) {
+            return self::API_URL_BSCSCAN;
         }
 
         return "https://{$net}.etherscan.io/api";
