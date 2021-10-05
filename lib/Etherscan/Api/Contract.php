a7e0c2c2aa760d12724851475fc8726e036a27c1
i.maslakoff <igormaslakoff@gmail.com>
2021-10-05 09:43:02 +0300

    contracts API update

--- lib/Etherscan/Api/Contract.php
+++ lib/Etherscan/Api/Contract.php
@@ -19,7 +19,8 @@ class Contract extends AbstractApi
      * @return array
      * @throws \Etherscan\Exception\ErrorException
      */
-    public function getABI($address) {
+    public function getABI($address)
+    {
         return $this->request->exec([
             'module' => "contract",
             'action' => "getabi",
@@ -35,8 +36,65 @@ class Contract extends AbstractApi
      *
      * @return array
      * @throws \Etherscan\Exception\ErrorException
+     *
+     * @deprecated deprecated since version 1.1.1
      */
-    public function getContractABI($address) {
+    public function getContractABI($address)
+    {
         return $this->getABI($address);
+    }
+
+    /**
+     * Get Contract Source Code for Verified Contract Source Codes
+     * Returns the Solidity source code of a verified smart contract.
+     *
+     * @param string $address Ether address.
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getSourceCode($address)
+    {
+        return $this->request->exec([
+            'module' => "contract",
+            'action' => "getsourcecode",
+            'address' => $address
+        ]);
+    }
+
+    /**
+     * Verify Source Code
+     * Check Source Code Verification Submission Status
+     *
+     * @param string $guid Source Code GUID
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function checkVerifyStatus($guid)
+    {
+        return $this->request->exec([
+            'module' => "contract",
+            'action' => "checkverifystatus",
+            'address' => $guid
+        ]);
+    }
+
+    /**
+     * Verify Proxy Contract
+     * Checking Proxy Contract Verification Submission Status using cURL
+     *
+     * @param string $guid GUID
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function checkProxyVerification($guid)
+    {
+        return $this->request->exec([
+            'module' => "contract",
+            'action' => "checkproxyverification",
+            'address' => $guid
+        ]);
     }
 }
