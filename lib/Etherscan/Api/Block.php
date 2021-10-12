5b86f57866b13824ef87f44e8c93beb189585ca7
i.maslakoff <igormaslakoff@gmail.com>
2021-10-12 09:45:10 +0300

    add Blocks pro API

--- lib/Etherscan/Api/Block.php
+++ lib/Etherscan/Api/Block.php
@@ -11,7 +11,7 @@ class Block extends AbstractApi
     /**
      * Get Block And Uncle Rewards by BlockNo.
      *
-     * @param int $blockNumber
+     * @param int $blockNumber block number to check block rewards
      *
      * @return array
      * @throws \Etherscan\Exception\ErrorException
@@ -28,7 +28,7 @@ class Block extends AbstractApi
     /**
      * Get Estimated Block Countdown Time by BlockNo.
      *
-     * @param int $blockNumber
+     * @param int $blockNumber block number to estimate time remaining to be mined
      *
      * @return array
      * @throws \Etherscan\Exception\ErrorException
@@ -45,8 +45,8 @@ class Block extends AbstractApi
     /**
      * Get Block Number by Timestamp
      *
-     * @param int $timestamp
-     * @param string $closest
+     * @param int $timestamp Unix timestamp in seconds
+     * @param string $closest the closest available block to the provided timestamp
      *
      * @return array
      * @throws \Etherscan\Exception\ErrorException
@@ -58,6 +58,111 @@ class Block extends AbstractApi
             'action' => "getblocknobytime",
             'timestamp' => $timestamp,
             'closest' => $closest
+        ]);
+    }
+
+    /**
+     * Get Daily Average Block Size
+     *
+     * @param string $startdate the starting date in yyyy-MM-dd format, eg. 2019-02-01
+     * @param string $enddate the ending date in yyyy-MM-dd format, eg. 2019-02-28
+     * @param string $sort 'asc' or 'desc'
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getDailyAverageBlockSize($startdate, $enddate, $sort = "asc")
+    {
+        return $this->request->exec([
+            'module' => "stats",
+            'action' => "dailyavgblocksize",
+            'startdate' => $startdate,
+            'enddate' => $enddate,
+            'sort' => $sort
+        ]);
+    }
+
+    /**
+     * Get Daily Block Count and Rewards
+     *
+     * @param string $startdate the starting date in yyyy-MM-dd format, eg. 2019-02-01
+     * @param string $enddate the ending date in yyyy-MM-dd format, eg. 2019-02-28
+     * @param string $sort 'asc' or 'desc'
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getDailyBlockCountAndRewards($startdate, $enddate, $sort = "asc")
+    {
+        return $this->request->exec([
+            'module' => "stats",
+            'action' => "dailyblkcount",
+            'startdate' => $startdate,
+            'enddate' => $enddate,
+            'sort' => $sort
+        ]);
+    }
+
+    /**
+     * Get Daily Block Rewards
+     *
+     * @param string $startdate the starting date in yyyy-MM-dd format, eg. 2019-02-01
+     * @param string $enddate the ending date in yyyy-MM-dd format, eg. 2019-02-28
+     * @param string $sort 'asc' or 'desc'
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getDailyBlockRewards($startdate, $enddate, $sort = "asc")
+    {
+        return $this->request->exec([
+            'module' => "stats",
+            'action' => "dailyblockrewards",
+            'startdate' => $startdate,
+            'enddate' => $enddate,
+            'sort' => $sort
+        ]);
+    }
+
+    /**
+     * Get Daily Average Time for A Block to be Included in the Ethereum Blockchain
+     *
+     * @param string $startdate the starting date in yyyy-MM-dd format, eg. 2019-02-01
+     * @param string $enddate the ending date in yyyy-MM-dd format, eg. 2019-02-28
+     * @param string $sort 'asc' or 'desc'
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getDailyAverageBlockTime($startdate, $enddate, $sort = "asc")
+    {
+        return $this->request->exec([
+            'module' => "stats",
+            'action' => "dailyavgblocktime",
+            'startdate' => $startdate,
+            'enddate' => $enddate,
+            'sort' => $sort
+        ]);
+    }
+
+    /**
+     * Get Daily Uncle Block Count and Rewards
+     *
+     * @param string $startdate the starting date in yyyy-MM-dd format, eg. 2019-02-01
+     * @param string $enddate the ending date in yyyy-MM-dd format, eg. 2019-02-28
+     * @param string $sort 'asc' or 'desc'
+     *
+     * @return array
+     * @throws \Etherscan\Exception\ErrorException
+     */
+    public function getDailyUncleBlockCountAndRewards($startdate, $enddate, $sort = "asc")
+    {
+        return $this->request->exec([
+            'module' => "stats",
+            'action' => "dailyuncleblkcount",
+            'startdate' => $startdate,
+            'enddate' => $enddate,
+            'sort' => $sort
         ]);
     }
 }
